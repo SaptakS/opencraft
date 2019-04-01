667c8c183d5d7cb4483d12bb41a4a73dfa2294fa
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-04-01 16:36:46 -0500

    Update ansible line timeout and circle CI output timeout. Add stacktrace to timeout error when running ansible playbook. Update branches to use Hawthorn and update logo assertion regex.

--- instance/tests/integration/factories/instance.py
+++ instance/tests/integration/factories/instance.py
@@ -58,8 +58,8 @@ class OpenEdXInstanceFactory(DjangoModelFactory):
     # or a release candidate tag will work.  We point both the edx-platform and the configuration
     # versions to the branch "integration" in our own forks.  These branches are based on the
     # corresponding openedx_release versions from upstream, but can contain custom modifications.
-    openedx_release = 'open-release/ginkgo.1'
+    openedx_release = 'open-release/hawthorn.1'
     configuration_source_repo_url = 'https://github.com/open-craft/configuration.git'
-    configuration_version = 'integration-ginkgo'
+    configuration_version = 'integration-hawthorn'
     edx_platform_repository_url = 'https://github.com/open-craft/edx-platform.git'
-    edx_platform_commit = 'opencraft-release/ginkgo.1'
+    edx_platform_commit = 'opencraft-release/hawthorn.1'
